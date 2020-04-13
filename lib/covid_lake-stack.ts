1bb922770c209b254850568646685ec5be0472e4
Brian Ross <bsross@amazon.com>
2020-04-13 18:07:50 -0400

    added tableau jhu data and enigma jhu timeseries data

--- lib/covid_lake-stack.ts
+++ lib/covid_lake-stack.ts
@@ -117,6 +117,211 @@ export class CovidLakeStack extends cdk.Stack {
       }
     });
 
+    const jhu_timeseries_table = new glue.CfnTable(this, 'jhu_timeseries', {
+      databaseName: db.databaseName,
+      catalogId: this.account,
+      tableInput: {
+        description: "Johns Hopkins University data on COVID-19 cases, sourced from Enigma",
+        name: "enigma_jhu_timeseries",
+        parameters: {
+          has_encrypted_data: false,
+          classification: "json", 
+          typeOfData: "file"
+        },
+        storageDescriptor: { 
+          columns: [
+            {
+              name: "uid",
+              type: "bigint",
+              comment: ""
+            },
+            {
+              name: "fips",
+              type: "string", 
+              comment: "state and county two digits code"
+            },
+            {
+              name: "iso2",
+              type: "string",
+              comment: ""
+            },
+            {
+              name: "iso3",
+              type: "string",
+              comment: ""
+            },
+            {
+              name: "code3",
+              type: "int",
+              comment: ""
+            },
+            {
+              name: "admin2",
+              type: "string", 
+              comment: "county name"
+            },
+            {
+              name: "latitude",
+              type: "double", 
+              comment: "location (latitude)"
+            }, 
+            {
+              name: "longitude",
+              type: "double", 
+              comment: "location (longitude)"
+            }, 
+            {
+              name: "province_state",
+              type: "string", 
+              comment: "province name or state name"
+            }, 
+            {
+              name: "country_region",
+              type: "string", 
+              comment: "country name or region name"
+            },
+            {
+              name: "date",
+              type: "string",
+              comment: "reporting date"
+            },
+            {
+              name: "confirmed",
+              type: "int", 
+              comment: "number of confirmed cases"
+            }, 
+            {
+              name: "deaths",
+              type: "int", 
+              comment: "number of deaths"
+            }, 
+            {
+              name: "recovered",
+              type: "int", 
+              comment: "number of recovered patients"
+            }, 
+            {
+              name: "active",
+              type: "int", 
+              comment: "number of active cases"
+            },
+            {
+              name: "combined_key",
+              type: "string", 
+              comment: "county name+state name+country name"
+            }
+          ],
+          compressed: false,
+          inputFormat: "org.apache.hadoop.mapred.TextInputFormat",
+          location: "s3://covid19-lake/enigma-jhu-timeseries/json/",
+          outputFormat: "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat",
+          serdeInfo: {
+            parameters: {
+              paths: "admin2,code3,confirmed,country_region,date,deaths,fips,iso2,iso3,latitude,longitude,province_state,recovered,uid"
+            },
+            serializationLibrary: "org.openx.data.jsonserde.JsonSerDe"
+          },
+          storedAsSubDirectories: false
+        },
+        tableType: "EXTERNAL_TABLE"
+      }
+    });
+
+    const tableau_jhu_table = new glue.CfnTable(this, 'tableau_jhu', {
+      databaseName: db.databaseName,
+      catalogId: this.account,
+      tableInput: {
+        description: "Johns Hopkins University data on COVID-19 cases, sourced from Tableau",
+        name: "tableau_jhu",
+        parameters: {
+          has_encrypted_data: false,
+          classification: "json", 
+          typeOfData: "file"
+        },
+        storageDescriptor: { 
+          columns: [
+            {
+              name: "case_type",
+              type: "string",
+              comment: "Confirmed, Deaths, etc."
+            },
+            {
+              name: "cases",
+              type: "bigint",
+              comment: "number of cases"
+            },
+            {
+              name: "difference",
+              type: "bigint",
+              comment: "difference in cases vs the previous day"
+            },
+            {
+              name: "date",
+              type: "string",
+              comment: "reporting date"
+            },
+            {
+              name: "country_region",
+              type: "string", 
+              comment: "country name or region name"
+            },
+            {
+              name: "province_state",
+              type: "string", 
+              comment: "province name or state name"
+            }, 
+            {
+              name: "admin2",
+              type: "string", 
+              comment: "county name"
+            },
+            {
+              name: "combined_key",
+              type: "string", 
+              comment: "county name+state name+country name"
+            },
+            {
+              name: "fips",
+              type: "string", 
+              comment: "state and county two digits code"
+            },
+            {
+              name: "lat",
+              type: "double", 
+              comment: "location (latitude)"
+            }, 
+            {
+              name: "long",
+              type: "double", 
+              comment: "location (longitude)"
+            }, 
+            {
+              name: "table_names",
+              type: "string",
+              comment: "source table name"
+            },
+            {
+              name: "prep_flow_runtime",
+              type: "string",
+              comment: "timestamp when the data was generated for this data load"
+            }
+          ],
+          compressed: false,
+          inputFormat: "org.apache.hadoop.mapred.TextInputFormat",
+          location: "s3://covid19-lake/tableau-jhu/json/",
+          outputFormat: "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat",
+          serdeInfo: {
+            parameters: {
+              paths: "Admin2,Case_Type,Cases,Combined_Key,Country_Region,Date,Difference,FIPS,Lat,Long,Prep_Flow_Runtime,Province_State,Table_Names"
+            },
+            serializationLibrary: "org.openx.data.jsonserde.JsonSerDe"
+          },
+          storedAsSubDirectories: false
+        },
+        tableType: "EXTERNAL_TABLE"
+      }
+    });
+
     /*
     new glue.CfnCrawler(this, 'jhu-crawler', {
       name: 'covid-enigma-jhu-crawler',
